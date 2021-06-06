842d2500a2f4443b31ec57d56df8a2ee822d6eb0
Harry Manoharan <harrymanoharan1@gmail.com>
2021-06-06 18:13:35 +0530

    Server - Documented Item Controller.

--- server/shoppingcartapi/src/main/java/com/assesment/shoppingcartapi/controller/ItemController.java
+++ server/shoppingcartapi/src/main/java/com/assesment/shoppingcartapi/controller/ItemController.java
@@ -15,6 +15,12 @@ import com.assesment.shoppingcartapi.dto.ItemPriceDto;
 import com.assesment.shoppingcartapi.entity.Item;
 import com.assesment.shoppingcartapi.enumerations.Measurement;
 
+/**
+ * The Endpoint of the Item API.
+ * 
+ * @author harrymanoharan
+ *
+ */
 @CrossOrigin
 @RestController
 public class ItemController {
@@ -22,16 +28,39 @@ public class ItemController {
 	@Autowired
 	private ItemService itemservice;
 	
+	/**
+	 * Returns the list of prices for all items per quantity provided.
+	 * 
+	 * example endpoint url: http://<host>:<port>/items/pricelist?qty=50
+	 * 
+	 * @param qty Requested quantity.
+	 * @return List of price information.
+	 */
 	@RequestMapping(path = "/items/pricelist",method = RequestMethod.GET)
 	public List<ItemPriceDto> getItemPriceList(@RequestParam int qty){
 		return itemservice.getPriceList(qty);
 	}
 	
+	/**
+	 * Returns the price of a given item, quantity and its unit of measure.
+	 * 
+	 * example endpoint url: http://<host>:<port>/items/1/price?qty=1&type=CARTON
+	 * 
+	 * @param id Item id.
+	 * @param qty Requested quantity.
+	 * @param type The unit of measure (unit/carton).
+	 * @return Returns the price information.
+	 */
 	@RequestMapping(path = "/items/{id}/price",method = RequestMethod.GET)
 	public ItemPriceDto getItemPrice(@PathVariable long id, @RequestParam int qty, @RequestParam Measurement type) {
 		return itemservice.getPrice(id, qty, type);
 	}
 	
+	/**
+	 * Returns a list of items.
+	 * 
+	 * @return Lsit of items.
+	 */
 	@RequestMapping(path ="/items", method = RequestMethod.GET)
 	public List<Item> getItems(){
 		return itemservice.getItems();
